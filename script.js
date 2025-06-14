134b557945e2a54cbcafa5f4a9e77ef1e1a30373
yolorys <yelarysseidin05@gmail.com>
2025-06-14 17:35:57 -0500

    Added the hovering effect

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ const container = document.querySelector(".container")
 for (let i = 0; i < 16; i++){
     for (let j = 0; j < 16; j++){
         const div = document.createElement("div");
+
+        div.addEventListener('mouseenter', () => {
+        div.style.backgroundColor = 'lightblue';
+        });
+
         container.appendChild(div);
     }
 }
